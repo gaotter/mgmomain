b2b95560f9f3431464156f89e7e2306aebcb28fa
gaotter <gaotter@gmail.com>
2023-02-19 16:31:18 +0100

    Remove bad code

--- Mgmo.Main/Mgmo.Main.WebApp/Pages/Index.cshtml.cs
+++ Mgmo.Main/Mgmo.Main.WebApp/Pages/Index.cshtml.cs
@@ -1,7 +1,5 @@
-﻿using Mgmo.Main.Article.Data.Config;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Options;
 
 namespace Mgmo.Main.WebApp.Pages
 {
@@ -9,12 +7,9 @@ namespace Mgmo.Main.WebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
-        public AppConfig AppConfig { get; set; } = null!;
-
-        public IndexModel(IOptionsSnapshot<AppConfig> appConfig, ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            AppConfig = appConfig.Value;
         }
 
         public void OnGet()
